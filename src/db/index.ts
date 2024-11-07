8e2540c9be145e385643a5e847979914f4c840c1
NikZak <nickzakirov@gmail.com>
2024-11-07 11:09:41 +0800

    added logging and docker-compose

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,41 +1,43 @@
-import { z } from "zod"
-import { createSqlTag } from "slonik"
-import Redis from "ioredis"
-import { createPool } from "slonik"
-import { createFieldNameTransformationInterceptor } from "slonik-interceptor-field-name-transformation"
+import { z } from "zod";
+import { createSqlTag } from "slonik";
+import Redis from "ioredis";
+import { createPool } from "slonik";
+import { createFieldNameTransformationInterceptor } from "slonik-interceptor-field-name-transformation";
 
 if (!process.env.REDIS_URL) {
-    throw Error("REDIS_URL env not set")
+  throw Error("REDIS_URL env not set");
 }
 export const redisClient = new Redis(process.env.REDIS_URL, {
-    maxRetriesPerRequest: null
-})
+  maxRetriesPerRequest: null,
+});
 
 const interceptors = [
-    createFieldNameTransformationInterceptor({
-        format: "CAMEL_CASE"
-    })
-]
+  createFieldNameTransformationInterceptor({
+    format: "CAMEL_CASE",
+  }),
+];
 
 const db = createPool(process.env.DB_URL!, {
-    interceptors,
-    ssl:
-        process.env.NODE_ENV === "development"
-            ? {
-                  rejectUnauthorized: false
-              }
-            : {
-                  rejectUnauthorized: true
-              }
-})
+  interceptors,
+  ssl:
+    process.env.NODE_ENV === "development"
+      ? {
+          rejectUnauthorized: false,
+        }
+      : {
+          rejectUnauthorized: true,
+        },
+});
+
+console.log("db pool created");
 
 export const sql = createSqlTag({
-    typeAliases: {
-        id: z.object({
-            id: z.number()
-        }),
-        void: z.object({}).strict()
-    }
-})
+  typeAliases: {
+    id: z.object({
+      id: z.number(),
+    }),
+    void: z.object({}).strict(),
+  },
+});
 
-export default db
+export default db;
